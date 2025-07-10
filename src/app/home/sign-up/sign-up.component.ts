7c664ad79213b4261c4658643077b4c6bfd890a3
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2025-07-10 12:34:23 +0200

    add a sign-up component

--- src/app/home/sign-up/sign-up.component.ts
+++ src/app/home/sign-up/sign-up.component.ts
@@ -1,52 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormControl } from '@angular/forms';
 import { SignInputComponent } from 'src/app/features/sign-input/sign-input.component';
 import { SharedService } from 'src/app/services/shared.service';
 
-
 @Component({
-  selector: 'app-sign-in',
-  standalone: true,
-  imports: [ReactiveFormsModule, SignInputComponent ],
-  templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  selector: 'app-sign-up',
+  imports: [ReactiveFormsModule, SignInputComponent],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
 })
-export class SignInComponent implements OnInit {
-  loginForm!: FormGroup;
+export class SignUpComponent implements OnInit {
+  signupForm!: FormGroup;
 
   constructor(private fb: FormBuilder, private sharedService: SharedService) {
   }
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
+    this.signupForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(60), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/)]],
       rememberMe: [false]
     })
-    this.loginForm.controls['email'].setValue(this.checkExistingEmail());
+    this.signupForm.controls['email'].setValue(this.checkExistingEmail());
   }
 
-  // Getter for email as FormControl
+  // Getter for identifier as FormControl
   get emailControl(): FormControl {
-    return this.loginForm.get('identifier') as FormControl;
+    return this.signupForm.get('identifier') as FormControl;
   }
 
   // Getter for password as FormControl
   get passwordControl(): FormControl {
-    return this.loginForm.get('password') as FormControl;
+    return this.signupForm.get('password') as FormControl;
   }
 
-  login() {
-    if (this.loginForm.valid) {
+  signup() {
+    if (this.signupForm.valid) {
       // Backend 
-      console.log(this.emailControl.value);
-
     }
   }
-  // check if a email adress was put into the get stark input field on the landing page - übernehmen
+
+  // check if a email adress was put into the get start input field on the landing page - übernehmen
   checkExistingEmail(): string {
     let email: string = this.sharedService.getIdentifier();
     if (email != '') return email;
     else return ''
   }
 }
+
+
